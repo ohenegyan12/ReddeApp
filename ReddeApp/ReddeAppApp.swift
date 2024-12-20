262a359aaa2c1e3b5ee7f78d2ba676cd5d96db8f
OGK <macbook@Ohenes-MacBook-Pro.local>
2024-12-20 22:08:19 +0000

    Initial commit

---
+++ ReddeApp/ReddeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReddeAppApp.swift
+//  ReddeApp
+//
+//  Created by OGK on 20/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct ReddeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ReddeAppTests/ReddeAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReddeAppTests.swift
+//  ReddeAppTests
+//
+//  Created by OGK on 20/12/2024.
+//
+
+import Testing
+@testable import ReddeApp
+
+struct ReddeAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
